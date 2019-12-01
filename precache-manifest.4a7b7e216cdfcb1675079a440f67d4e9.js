58ab281e561ac811228d1730b2d80eed0331baf3
Vincent Siao <vincent@asana.com>
2019-11-30 22:02:28 -0500

    Updates

---
+++ precache-manifest.4a7b7e216cdfcb1675079a440f67d4e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040ef447fa6ed2e3676751552a9c0e65",
+    "url": "/yeunglovin/index.html"
+  },
+  {
+    "revision": "9ed1acc677be8c09ce40",
+    "url": "/yeunglovin/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "94077b58252b9b17a5b0",
+    "url": "/yeunglovin/static/js/2.6cd6a31c.chunk.js"
+  },
+  {
+    "revision": "9ed1acc677be8c09ce40",
+    "url": "/yeunglovin/static/js/main.4bacbf4c.chunk.js"
+  },
+  {
+    "revision": "0f5edc7e668184e5685b",
+    "url": "/yeunglovin/static/js/runtime-main.881da43c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/yeunglovin/static/media/logo.25bf045c.svg"
+  }
+]);
