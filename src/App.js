2fb52f5570a1239ecb9c1e20763c0ba5bfbcf13f
Vincent Siao <vincent@asana.com>
2019-12-30 10:01:27 -0800

    Add a splash page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { HashRouter, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 
 function Home() { return <div className="Home" />; }
 function About() { return <div className="About" />; }
 
 function App() {
-  return (
+  return <>
     <HashRouter basename="/">
-      <div className="App">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/details">Details</Link></li>
+      <div className="Splash">
+        <h1 className="Splash-title">stephanie &<br />vincent</h1>
+        {/* <p>10.10.2020</p> */}
+        <ul className="Splash-nav">
+          <li className="Splash-navItem"><Link to="/rsvp" className="Splash-navLink">rsvp</Link></li>
+          <li className="Splash-navItem"><Link to="/schedule" className="Splash-navLink">schedule</Link></li>
+          <li className="Splash-navItem"><Link to="/stuff" className="Splash-navLink">stuff</Link></li>
         </ul>
+      </div>
+      <div className="App">
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
       </div>
     </HashRouter>
-  );
+  </>;
 }
 
 export default App;
