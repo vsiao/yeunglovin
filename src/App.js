a225d389f8af9565f738ace3ae72c9e145370c43
Stephanie Yeung <stephanie@habitat-labs.com>
2020-01-26 21:04:24 -0500

    Splash design

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React from "react";
-import { HashRouter, Route, Link } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
+import './App.css';
 
-function Home() { return <div className="Home" />; }
-function About() { return <div className="About" />; }
+function Home() {
+  return <div className="Home" />;
+}
+function About() {
+  return <div className="About" />;
+}
 
 function App() {
-  return <>
-    <HashRouter basename="/">
-      <div className="Splash">
-        <h1 className="Splash-title">stephanie &<br />vincent</h1>
-        {/* <p>10.10.2020</p> */}
-        <ul className="Splash-nav">
-          <li className="Splash-navItem"><Link to="/rsvp" className="Splash-navLink">rsvp</Link></li>
-          <li className="Splash-navItem"><Link to="/schedule" className="Splash-navLink">schedule</Link></li>
-          <li className="Splash-navItem"><Link to="/stuff" className="Splash-navLink">stuff</Link></li>
-        </ul>
-      </div>
-      <div className="App">
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </div>
-    </HashRouter>
-  </>;
+  return (
+    <>
+      <HashRouter basename="/">
+        <div className="Splash">
+          <div className="Splash-wrapper">
+            <h1 className="Splash-title">10.10.20</h1>
+            <h2 className="Splash-subtitle">Stephanie & Vincent</h2>
+            <div className="Splash-sticker">(are getting married)</div>
+            <div className="Splash-okay">
+              {getCelebrate()} <span className="Splash-arrow">↓</span>
+            </div>
+          </div>
+          <ul className="Splash-nav">
+            <li className="Splash-navItem">
+              <Link to="/rsvp" className="Splash-navLink">
+                rsvp
+              </Link>
+            </li>
+            <li className="Splash-navItem">
+              <Link to="/schedule" className="Splash-navLink">
+                schedule
+              </Link>
+            </li>
+            <li className="Splash-navItem">
+              <Link to="/stuff" className="Splash-navLink">
+                stuff
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
+
+function getCelebrate() {
+  const words = ['Yay!', 'Coolbeans', 'Okay, so what now?', 'And…?', 'Tell me more'];
+  const randIndex = Math.floor(Math.random() * Math.floor(words.length));
+  return words[randIndex];
+}
